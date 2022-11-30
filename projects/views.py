8f32f86d4c09f835ea425d8bb5acdfd1e2b38f59
MohsenSami <mohsensami88@gmail.com>
2022-11-30 21:33:16 +0330

    Project and Review and Tag models added and get projects from db

--- projects/views.py
+++ projects/views.py
@@ -1,32 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
-
-projectsList = [
-    {
-        'id': 1,
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': 2,
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': 3,
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
 
 def projects(request):
+    projectsList = Project.objects.all()
     context = {'projects': projectsList}
     return render(request, 'projects/list.html', context)
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single.html', {'project': projectObj})
