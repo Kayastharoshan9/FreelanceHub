bc747327395c0ba258ba1d72ea2b525ebc0e5339
MohsenSami <mohsensami88@gmail.com>
2022-11-30 23:13:39 +0330

    Projects CRUD , Logic without ui

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.projects, name='projects'),
+    path('create-project/', views.CreateProject.as_view(), name='create-project'),
+    path('update-project/<str:pk>', views.UpdateProject.as_view(), name='update-project'),
+    path('delete-project/<str:pk>', views.DeleteProject.as_view(), name='delete-project'),
     path('<str:pk>/', views.project, name='project'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Project
+from django.views import View
+from .forms import ProjectForm
 
 
 def projects(request):
@@ -11,3 +12,54 @@ def projects(request):
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single.html', {'project': projectObj})
+
+
+class CreateProject(View):
+    form_class = ProjectForm
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, 'projects/project_form.html', {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+
+class UpdateProject(View):
+    form_class = ProjectForm
+
+    def setup(self, request, *args, **kwargs):
+        self.project_instance = get_object_or_404(Project, pk = kwargs['pk'])
+        return super().setup(request, *args, **kwargs)
+
+    def get(self, request, pk):
+        project = self.project_instance
+        form = self.form_class(instance=project)
+        return render(request, 'projects/project_form.html', {'form': form})
+
+    def post(self, request, pk):
+        project = self.project_instance
+        form = self.form_class(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+
+class DeleteProject(View):
+    form_class = ProjectForm
+
+    def setup(self, request, *args, **kwargs):
+        self.project_instance = get_object_or_404(Project, pk = kwargs['pk'])
+        return super().setup(request, *args, **kwargs)
+
+    def get(self, request, pk):
+        obj = self.project_instance
+        return render(request, 'projects/delete_template.html', {'obj': obj})
+
+    def post(self, request, pk):
+        project = self.project_instance
+        project.delete()
+        return redirect('projects')
