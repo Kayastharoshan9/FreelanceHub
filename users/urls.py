abd59234f89b4219a76a296fffaa12d0df9bd0ac
MohsenSami <mohsensami88@gmail.com>
2022-12-01 18:20:26 +0330

    Login Register Logout

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.LoginUserView.as_view(), name="login"),
+    path('logout/', views.LogoutUserView.as_view(), name="logout"),
+    path('register/', views.RegisterUserView.as_view(), name="register"),
+    
     path('', views.ProfileListView.as_view(), name="profiles"),
     path('profile/<str:pk>/', views.ProfileDetailView.as_view(), name="profile-detail"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
 from django.views import View
 from .models import Profile
+from .forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class LoginUserView(View):
+    page = 'login'
+    def get(self, request):
+        context = {'page': self.page}
+        return render(request, 'users/login_register.html', context)
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('/')
+        return super().dispatch(request, *args, **kwargs)
+    
+    def post(self, request):
+        username = request.POST['username'].lower()
+        password = request.POST['password']
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exist')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, 'Username OR password is incorrect')
+        return render(request, 'users/login_register.html', {'page': self.page})
+
+
+class LogoutUserView(LoginRequiredMixin, View):
+    def get(self, request):
+        logout(request)
+        messages.info(request, 'User was logged out!')
+        return redirect('login')
+
+
+class RegisterUserView(View):
+    page = 'register'
+    form = CustomUserCreationForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('/')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        context = {'page': self.page, 'form': self.form}
+        return render(request, 'users/login_register.html', context)
+
+    def post(self, request):
+        form = self.form(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'User account was created!')
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, 'An error has occurred during registration')
+        return render(request, 'users/login_register.html', {'page': self.page, 'form': form})
 
 
 class ProfileListView(View):
