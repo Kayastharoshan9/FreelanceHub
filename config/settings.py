d775c1e2a5309c63160eba08cd7088bc8264a84e
MohsenSami <mohsensami88@gmail.com>
2022-12-16 17:18:19 +0330

    seperate app email pass from settings.py

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 
+from .env import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,7 +130,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mohsensami88@gmail.com'
-EMAIL_HOST_PASSWORD = 'pocarnqxjohiksak'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
